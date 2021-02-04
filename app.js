4df45f9a3a243aa80397e684443cb876c03f9909
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-05 00:10:52 +0600

    final code complete

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-// const button = document.querySelector('#searchButton');
+const button = document.querySelector('#searchButton');
 const inputLocation = document.querySelector('#inputLocation');
+const weatherImageUrl = `http://openweathermap.org/img/wn/`
 const weatherImage = document.querySelector('#weatherImage');
 const locationName = document.querySelector('#locationName');
 const locationTemperature = document.querySelector('#locationTemperature');
-const locationObserve = document.querySelector('#locationObserve');
+const locationObserve = document.querySelector('#locationObserve'); 
 
-const button = document.querySelector('#searchButton').addEventListener('click',function(){
-    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f75ab83bb1cb565647a4e6b0c8499e2c')
+button.addEventListener('click',function(){
+    let cityName = inputLocation.value;
+    //use API js code
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=f75ab83bb1cb565647a4e6b0c8499e2c`  
+    fetch(url)
     .then(res => res.json())
     .then(data => {
-    const weatherDetails = data.cod[0];
-
+        
+     locationName.innerText = data.name;
+     locationTemperature.innerText = Math.round((data.main.temp) - 273.15)
+     locationObserve.innerText = data.weather[0].main;
+     weatherImage.setAttribute('src', `${weatherImageUrl}${data.weather[0].icon}.png`)
     })
 .catch(error => alert('wrong city name !'))
 })
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=f75ab83bb1cb565647a4e6b0c8499e2c`  
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        
+     locationName.innerText = data.name;
+     locationTemperature.innerText = Math.round((data.main.temp) - 273.15)
+     locationObserve.innerText = data.weather[0].main;
+     weatherImage.setAttribute('src', `${weatherImageUrl}${data.weather[0].icon}.png`)
+    })
+.catch(error => alert('wrong city name !'))
