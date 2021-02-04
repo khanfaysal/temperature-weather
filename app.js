3a0e53d3e3c9a8a619532adafb7624034972c3a2
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-04 15:16:42 +0600

    code running

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const button = document.querySelector('#searchButton');
+// const button = document.querySelector('#searchButton');
 const inputLocation = document.querySelector('#inputLocation');
 const weatherImage = document.querySelector('#weatherImage');
 const locationName = document.querySelector('#locationName');
 const locationTemperature = document.querySelector('#locationTemperature');
 const locationObserve = document.querySelector('#locationObserve');
 
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f75ab83bb1cb565647a4e6b0c8499e2c')
-.then(res => res.json())
-.then(data => {
-    console.log(data);
+const button = document.querySelector('#searchButton').addEventListener('click',function(){
+    fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f75ab83bb1cb565647a4e6b0c8499e2c')
+    .then(res => res.json())
+    .then(data => {
+    const weatherDetails = data.cod[0];
+
+    })
+.catch(error => alert('wrong city name !'))
 })
